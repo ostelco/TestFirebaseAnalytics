3bc69e147286bb6546fc872f64ee3d711a7f11f6
Håvard Norén <havardnoren@gmail.com>
2019-06-18 15:38:21 +0200

    Initial Commit

---
+++ TestFirebaseAnalytics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestFirebaseAnalytics
+//
+//  Created by mac on 6/18/19.
+//  Copyright © 2019 NorensIKT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
