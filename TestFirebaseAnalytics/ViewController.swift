38baf05323818ef810c152f0a08c1bc52dfb5585
Håvard Norén <havardnoren@gmail.com>
2019-06-18 16:09:45 +0200

    test firebase analytics custom event

--- TestFirebaseAnalytics/ViewController.swift
+++ TestFirebaseAnalytics/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import FirebaseAnalytics
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Analytics.logEvent("test_event", parameters: [:])
     }
 
 
